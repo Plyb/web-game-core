55598e144fcd7628340555022cd219f4f61a3298
Koby Lewis <kobyjlewis@gmail.com>
2022-05-29 17:09:51 -0600

    reformat index.ts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -60,41 +60,41 @@ function getUserId(): string | null {
 export function setUpVueApp(customApp?: DefineComponent) {
 
 	type TaggedElement = Element & {
-	eventMap: {
-		[key: string]: (event: MouseEvent) => void
-	}
+		eventMap: {
+			[key: string]: (event: MouseEvent) => void
+		}
 	}
 	function createMouseOutsideDirective(eventType: 'mouseup' | 'click' | 'touchend') {
-	return {
-		beforeMount: function (el: TaggedElement, binding: Vue.DirectiveBinding) {
-		function clickOutsideEvent(event: MouseEvent | TouchEvent) {
-			// here I check that click was outside the el and his children
-			const target = (function() {
-				if (event instanceof MouseEvent) {
-					return event.target;
-				} else {
-					const touch = event.changedTouches[0];
-					if (touch) {
-						return document.elementFromPoint(touch.clientX, touch.clientY);
+		return {
+			beforeMount: function (el: TaggedElement, binding: Vue.DirectiveBinding) {
+				function clickOutsideEvent(event: MouseEvent | TouchEvent) {
+					// here I check that click was outside the el and his children
+					const target = (function() {
+						if (event instanceof MouseEvent) {
+							return event.target;
+						} else {
+							const touch = event.changedTouches[0];
+							if (touch) {
+								return document.elementFromPoint(touch.clientX, touch.clientY);
+							}
+						}
+					})()
+					if (window.document.contains(target as Node)
+						&& !(el == target || el.contains(target as Node))) {
+						// and if it did, call method provided in attribute value
+						binding.value();
 					}
 				}
-			})()
-			if (window.document.contains(target as Node)
-				&&!(el == target || el.contains(target as Node))) {
-				// and if it did, call method provided in attribute value
-				binding.value();
-			}
+				if (!el.eventMap) {
+					el.eventMap = {};
+				}
+				el.eventMap[eventType] = clickOutsideEvent;
+				document.addEventListener(eventType, clickOutsideEvent)
+			},
+			unmounted: function (el: any) {
+				document.removeEventListener(eventType, el.eventMap[eventType])
+			},
 		}
-		if (!el.eventMap) {
-			el.eventMap = {};
-		}
-		el.eventMap[eventType] = clickOutsideEvent;
-		document.addEventListener(eventType, clickOutsideEvent)
-		},
-		unmounted: function (el: any) {
-		document.removeEventListener(eventType, el.eventMap[eventType])
-		},
-	}
 	}
 
 	const app = customApp || createApp(App);
